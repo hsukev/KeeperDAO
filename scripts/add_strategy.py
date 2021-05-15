f627873b7a3d0a974cfafe01d5ca0c9f231eb15f
Kevin Hsu <khsu@medium.com>
2021-05-14 22:46:42 -0700

    add_strategy script

---
+++ scripts/add_strategy.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+from brownie import accounts, config, network, project, web3
+from eth_utils import is_checksum_address
+import click
+
+API_VERSION = config["dependencies"][0].split("@")[-1]
+Vault = project.load(
+    Path.home() / ".brownie" / "packages" / config["dependencies"][0]
+).Vault
+
+
+def get_address(msg: str, default: str = None) -> str:
+    val = click.prompt(msg, default=default)
+
+    # Keep asking user for click.prompt until it passes
+    while True:
+
+        if is_checksum_address(val):
+            return val
+        elif addr := web3.ens.address(val):
+            click.echo(f"Found ENS '{val}' [{addr}]")
+            return addr
+
+        click.echo(
+            f"I'm sorry, but '{val}' is not a checksummed address or valid ENS record"
+        )
+        # NOTE: Only display default once
+        val = click.prompt(msg)
+
+
+def main():
+    print(f"You are using the '{network.show_active()}' network")
+    dev = accounts.load(click.prompt("Account", type=click.Choice(accounts.load())))
+    print(f"You are using: 'dev' [{dev.address}]")
+
+    vault = Vault.at(get_address("Deployed Vault: "))
+    strategy = get_address("Deployed strategy: ")
+
+    if input("Add Strategy? y/[N]: ").lower() != "y":
+        return
+
+    vault.addStrategy(strategy, 0, 0, 1000, {"from": dev})
