6aa44efee6d824384266a8953ff185f278c7d065
Kevin Hsu <khsu@medium.com>
2021-05-17 18:11:15 -0700

    contribution edit

--- scripts/set_contributors.py
+++ scripts/set_contributors.py
@@ -12,22 +12,21 @@ def main():
     strategyRookWETH = "0xFc84A04478Ffe0B48e46048f4E933A51F4016289"
     kev = "0x1F93b58fb2cF33CfB68E73E94aD6dD7829b1586D"
     wavey = "0x6AFB7c9a6E8F34a3E0eC6b734942a5589A84F44C"
-    matt = "0xd9c68eb096db712FFE15ede78B3D020903F8aa30"
     sharerv4.setContributors(
         strategyRookDAI,
-        [kev, ms, wavey, matt],
-        [800, 100, 90, 10],
+        [kev, ms, wavey],
+        [810, 100, 90],
         {"from": ms}
     )
     sharerv4.setContributors(
         strategyRookUSDC,
-        [kev, ms, wavey, matt],
-        [800, 100, 90, 10],
+        [kev, ms, wavey],
+        [810, 100, 90],
         {"from": ms}
     )
     sharerv4.setContributors(
         strategyRookWETH,
-        [kev, ms, wavey, matt],
-        [800, 100, 90, 10],
+        [kev, ms, wavey],
+        [810, 100, 90],
         {"from": ms}
     )
